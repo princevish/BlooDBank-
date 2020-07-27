2dc125c4feea73e627e5297bede9917729289b06
Prince Vishwakarma <princevworld@gmail.com>
2020-07-27 20:24:50 +0530

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.utils.encoding import python_2_unicode_compatible
+
 
 
 class ProfileModel(models.Model):
